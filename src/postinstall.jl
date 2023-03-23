a0b7991ca469481cf99668a87f764db6d3ac9110
Markus Petters <mdpetter@ncsu.edu>
2023-03-23 10:36:38 -0400

    ensure use of distro Python

--- src/postinstall.jl
+++ src/postinstall.jl
@@ -1,4 +1,3 @@
-using Conda
-Conda.pip_interop(true)
-Conda.pip("install", "/usr/local/share/ids/bindings/python/wheel/ids_peak_ipl-1.4.0.0-cp310-cp310-linux_x86_64.whl")
-Conda.pip("install", "/usr/local/share/ids/bindings/python/wheel/ids_peak-1.4.2.2-cp310-cp310-linux_x86_64.whl")
+ENV["PYTHON"] = "/usr/bin/python3"
+using Pkg
+Pkg.build("PyCall")
